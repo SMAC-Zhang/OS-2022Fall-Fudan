fb941957d6e426497b56470ac4e1312c4ad7ff6d
smac <20307130152@fudan.edu.cn>
2022-09-25 12:36:19 +0800

    lab2  o(1)

--- src/kernel/mem.c
+++ src/kernel/mem.c
@@ -26,63 +26,54 @@ define_early_init(init_page) {
 
 
 struct my_list_node {
-    struct my_list_node *next;
+    u64    length;
+    struct my_list_node *prev,  *next;
 };
 
 // add and merge
-// if merge, return new_addr
+// if merge, return new pointer
 // or return NULL
 struct my_list_node* add_to_list(struct my_list_node** head, struct my_list_node* node, u64 size) {
     if (*head == NULL) {
         node->next = NULL;
+        node->prev = NULL;
         *head = node;
+        node->length = size * 2 + 1;
         return NULL;
     }
 
-    if ((u64)(*head) > (u64)node) {
-        node->next = *head;
-        *head = node;
-        if ((((u64)(node) & size) == 0) && get_page_num((*head)->next) == get_page_num(node) && (u64)((*head)->next) - (u64)(node) == size) {
-            *head = (*head)->next->next;
-            return node;
+    struct my_list_node* next_addr = (void*)node + size;
+    if (((u64)node & size) == 0 && get_page_num(next_addr) == get_page_num(node) && next_addr->length == size * 2 + 1) {
+        if (next_addr == *head) {
+            *head = (*head)->next;
+        } else {
+            next_addr->prev->next = next_addr->next;
         }
-        return NULL;
-    }
-
-    struct my_list_node *p = *head, *prev = *head;
-    while (p) {
-        if ((u64)p < (u64)node && (
-        (p->next && (u64)(p->next) > (u64)node)
-        || (p->next == NULL))
-        ) {
-            node->next = p->next;
-            p->next = node;
-            break;
-        }
-        p = p->next;
-        if (prev->next != p) {
-            prev = prev->next;
-        }
-    }
-    if ((((u64)(p) & size) == 0) && get_page_num(node) == get_page_num(p) && (u64)node - (u64)p == size) {
-        if (p == *head) {
-            *head = node->next;
-            return p;
-        }
-        prev->next = node->next;
-        return p;
-    }
-    if ((((u64)(node) & size) == 0) && node->next && get_page_num(node) == get_page_num(node->next) && (u64)(node->next) - (u64)node == size) {
-        p->next = node->next->next;
+        next_addr->length = 0;
         return node;
     }
-    
-    // node->next = (*head);
-    // (*head) = node;
 
+    struct my_list_node* prev_addr = (void*)node - size;
+    if (((u64)prev_addr & size) == 0 && get_page_num(prev_addr) == get_page_num(node) && prev_addr->length == size * 2 + 1) {
+        if (prev_addr == *head) {
+            (*head) = (*head)->next;
+        } else {
+            prev_addr->prev->next = prev_addr->next;
+        }
+        prev_addr->length = 0;
+        return prev_addr;
+    }
+    
+    (*head)->prev = node;
+    node->next = (*head);
+    node->prev = NULL;
+    (*head) = node;
+    node->length = size * 2 + 1;
     return NULL;
 }
 
+// a function that wraps the add operation
+// merge up the way if it could
 void wrap_add(struct my_list_node* node, u64 log_size) {
     auto p = node;
     auto i = log_size;
@@ -90,9 +81,10 @@ void wrap_add(struct my_list_node* node, u64 log_size) {
         if (i == 12) {
             kfree_page(p);
             // static int cnt = 0;
-            // printk("%d ", ++cnt);
+            // printk("%d ", ++cnt); //print the number of page_free because of merge
             return;
         }
+        p->length = 0;
         p = add_to_list(&free_list[cpuid()][i], p, 1 << i);
         i++;
     } while (p != NULL);
@@ -125,6 +117,7 @@ void kfree_page(void* p) {
     add_to_queue(&pages, (QueueNode*)p);
 }
 
+
 u64 __log2(i64 num) {
     i64 ret = 0;
     i64 t = num;
@@ -144,18 +137,16 @@ u64 __log2(i64 num) {
 void* kalloc(isize size) {
     size += 8;
 
-    if ((1 << __log2(size)) - size >= 32) {
-        size = (size + 31) / 32 * 32;
-    } else {
-        size = (size + 7) / 8 * 8;
-    }
+    size = (size + 31) / 32 * 32;
 
-    u64 pos = __log2(size);
-    u64 log_size = __log2(size);
+    // find a free block
+    u64 pos = __log2(size), log_size = __log2(size);
     while (pos < 12 && free_list[cpuid()][pos] == NULL) {
         pos++;
     }
     
+    // if found, get it
+    // or kalloc a new page
     void *ret_addr;
     if (pos != 12) {
         ret_addr = fetch_from_list(&free_list[cpuid()][pos]);
@@ -163,22 +154,22 @@ void* kalloc(isize size) {
         ret_addr = kalloc_page();
     }
 
-    void *rest_addr = ret_addr + size;
-    u64 rest_size = (1 << pos) - size;
-    log_size++;
-    for (int i = 0; i < 12; ++i) {
-        if ((rest_size >> i) & 1) {
-            wrap_add((struct my_list_node*)rest_addr, (u64)i);
-            rest_addr += 1 << i;
-        }
-    }
-    *((u64*) ret_addr) = size;
-
-    // for (int i = pos - 1; i >= (i64)log_size; --i) {
-    //     wrap_add((struct my_list_node*)(ret_addr + (1 << i)), (u64)i);
-    //     //add_to_list(&free_list[cpuid()][i], (struct my_list_node*)(ret_addr + (1 << i)));
+    // cut off the block to have a fit space 
+    // the rest space would be added to the list
+    // void *rest_addr = ret_addr + size;
+    // u64 rest_size = (1 << pos) - size;
+    // for (int i = 0; i < 12; ++i) {
+    //     if ((rest_size >> i) & 1) {
+    //         wrap_add((struct my_list_node*)rest_addr, (u64)i);
+    //         rest_addr += 1 << i;
+    //     }
     // }
-    // *((u64*) ret_addr) = log_size;
+    // *((u64*) ret_addr) = size * 2;
+
+    for (int i = pos - 1; i >= (i64)log_size; --i) {
+        wrap_add((struct my_list_node*)(ret_addr + (1 << i)), (u64)i);
+    }
+    *((u64*) ret_addr) = log_size;
     
     return ret_addr + 8;
 }
@@ -186,16 +177,13 @@ void* kalloc(isize size) {
 void kfree(void* p) {
     p -= 8;
 
-    //u64 log_size = *(u64*) (p);
-    //wrap_add((struct my_list_node*)p, log_size);
-    
-    //add_to_list(&free_list[cpuid()][log_size], (struct my_list_node*)p);
-
-    u64 size = *(u64*) (p);
-    for (int i = 11; i >= 0; --i) {
-        if ((size >> i) & 1) {
-            wrap_add((struct my_list_node*)p, i);
-            p += 1 << i;
-        }
-    }
+    u64 log_size = *(u64*) (p);
+    wrap_add((struct my_list_node*)p, log_size);
+    // u64 size = *(u64*) (p) / 2;
+    // for (int i = 11; i >= 0; --i) {
+    //     if ((size >> i) & 1) {
+    //         wrap_add((struct my_list_node*)p, i);
+    //         p += 1 << i;
+    //     }
+    // }
 }
